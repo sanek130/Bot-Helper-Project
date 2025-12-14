020b66c58fefcba781ded495a2c42d3e26756202
sanek130 <sashshih@gmail.com>
2025-12-14 14:40:35 +0300

    config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const config = {
   telegramToken: process.env.BOT_TOKEN,
-  mongodbUri: process.env.MONGODB_URI,
+  mongodbUri: process.env.MONGODB_URI, // ← вот здесь!
   adminChatIds: [5191412364, 369745517]
 };
